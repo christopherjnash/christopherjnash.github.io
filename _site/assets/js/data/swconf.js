f4678277be300a3c230aea0a29a8507b14aa3ba5
christopherjnash <39203413+christopherjnash@users.noreply.github.com>
2025-04-04 22:33:56 -0400

    Adding files -- favicon, etc

---
+++ _site/assets/js/data/swconf.js
@@ -0,0 +1,24 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1743819791',resources: [
+      '/assets/css/jekyll-theme-chirpy.css',
+      '/',
+      
+        '/about/',
+      
+
+      
+      
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+      ]
+    },
+
+    purge: false
+  
+};
+
