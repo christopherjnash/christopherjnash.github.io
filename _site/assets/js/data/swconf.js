f5aa4a430a2392020feafb36109f098717829afb
Chris Nash <cnash6@gmail.com>
2026-01-06 14:00:52 -0500

    Update metadata timestamps and cache name for consistency across site files

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1767726011',resources: [
+    cacheName: 'chirpy-1767726040',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
