7df434d69e77bd1e38f4e3558a80c382cdcc6142
Chris Nash <cnash6@gmail.com>
2026-01-06 14:10:59 -0500

    Update site metadata and normalize browser tab titles across pages

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1767726040',resources: [
+    cacheName: 'chirpy-1767726634',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
