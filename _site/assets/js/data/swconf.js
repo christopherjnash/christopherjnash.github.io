45ab9fa91e527f5d823232c025f986b58c152b59
Chris Nash <cnash6@gmail.com>
2026-01-06 14:14:27 -0500

    Remove pages-deploy duplicate workflow

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1767726634',resources: [
+    cacheName: 'chirpy-1767726681',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
