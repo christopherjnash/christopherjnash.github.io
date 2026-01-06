68e613cbbd1bfb8aab9b57c5783045955c594f4b
Chris Nash <cnash6@gmail.com>
2026-01-06 14:42:34 -0500

    Update deploy workflow

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1767728040',resources: [
+    cacheName: 'chirpy-1767728543',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
