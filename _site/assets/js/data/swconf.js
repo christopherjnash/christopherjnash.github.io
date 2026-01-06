f122c462d8f315989dfbb8782efe9c6d7bd27549
Chris Nash <cnash6@gmail.com>
2026-01-06 14:35:42 -0500

    Keep sidebar name as "Chris Nash"

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1767726681',resources: [
+    cacheName: 'chirpy-1767728040',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
